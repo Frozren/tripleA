464ab02a3e085015bb09acfe6b581d07954d3ac6
AntoineVrinat <antoine.vrinat.91@gmail.com>
2020-04-30 10:22:15 +0200

    SpringIDAOPlayer.java

--- tripleABack/src/main/java/fr/formation/dao/IDAOPlayer.java
+++ tripleABack/src/main/java/fr/formation/dao/IDAOPlayer.java
@@ -1,6 +1,9 @@
 package fr.formation.dao;
+import fr.formation.daobackup.DAO;
+import fr.formation.model.Player;
 
-public interface IDAOPlayer{
+public interface IDAOPlayer extends DAO<Player, Integer> {
 	
-}
+	public Player findByName(String name);
 
+}
