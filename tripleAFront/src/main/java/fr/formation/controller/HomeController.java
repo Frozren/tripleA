be47cf402ceb1d28004dd7295dd8da8ea77df943
AdrienB <boubee.adrien@gmail.com>
2020-05-06 14:25:46 +0200

    zaezae

---
+++ tripleAFront/src/main/java/fr/formation/controller/HomeController.java
@@ -0,0 +1,10 @@
+package fr.formation.controller;
+
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+//@RequestMapping("/home") // PREFIXER TOUS LES MAPPINGS DE CETTE CLASSE
+public class HomeController {
+
+}
