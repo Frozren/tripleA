e8ff43b648f95385a0baeefc37c792a4b7cc5c8b
AdrienB <boubee.adrien@gmail.com>
2020-05-14 09:47:28 +0200

    beforeSecurity

--- tripleA-boot/src/main/java/fr/formation/controller/HomeController.java
+++ tripleA-boot/src/main/java/fr/formation/controller/HomeController.java
@@ -14,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import fr.formation.dao.IDAOHistory;
 import fr.formation.model.History;
@@ -29,6 +31,8 @@ public class HomeController {
 	@GetMapping("/home")
 	public String findAllHistory(Model model, HttpSession session) {
 		session.setAttribute("blockRefresh", false);
+		session.setAttribute("triBool", true);
+		session.setAttribute("triColonne", "colDate");
 		List<History> history = this.daoHistory.findAll();
 		List<History> histoT = new ArrayList<History>(history);
 		List<History> histoD = new ArrayList<History>(history);
@@ -79,5 +83,59 @@ public class HomeController {
 	public String resetHisto() {
 		this.daoHistory.deleteAll();
 		return "redirect:/home";
+	}
+	
+	@PostMapping("/history-ajax")
+	public String historyAjax(@RequestBody String colonne, Model model, HttpSession session) {
+		List<History> history = this.daoHistory.findAll();
+		model.addAttribute("history", history);
+		
+		Boolean triBool = (Boolean) session.getAttribute("triBool");
+		String triColonne = (String) session.getAttribute("triColonne");
+		
+		if (colonne.equals(triColonne)) {
+			triBool = (triBool) ? false : true;
+		} else {
+			triBool = true;
+		}
+		
+		if (colonne.contains("Date")) {
+			if (triBool) {
+				history.sort(Comparator.comparing(History::getDateEnd).reversed());
+			} else {
+				history.sort(Comparator.comparing(History::getDateEnd));
+			}
+		} else if (colonne.contains("Joueur")) {
+			if (triBool) {
+				history.sort(Comparator.comparing(History::getName));
+			} else {
+				history.sort(Comparator.comparing(History::getName).reversed());
+			}
+		} else if (colonne.contains("Etage")) {
+			if (triBool) {
+				history.sort(Comparator.comparing(History::getPhase));
+			} else {
+				history.sort(Comparator.comparing(History::getPhase).reversed());
+			}
+		} else if (colonne.contains("Infligés")) {
+			if (triBool) {
+				history.sort(Comparator.comparing(History::getDmgDealt));
+			} else {
+				history.sort(Comparator.comparing(History::getDmgDealt).reversed());
+			}
+		} else if (colonne.contains("Reçus")) {
+			if (triBool) {
+				history.sort(Comparator.comparing(History::getDmgTaken));
+			} else {
+				history.sort(Comparator.comparing(History::getDmgTaken).reversed());
+			}
+		}
+		
+		session.setAttribute("triColonne", colonne);
+		session.setAttribute("triBool", triBool);
+		
+		System.out.println(triBool);
+		
+		return "home-history";
 	}
 }
