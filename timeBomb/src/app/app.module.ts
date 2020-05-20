7b3c4bbef49b8e95e71073370701f483b2fa3dbc
AdrienB <boubee.adrien@gmail.com>
2020-05-20 13:43:03 +0200

    route

--- timeBomb/src/app/app.module.ts
+++ timeBomb/src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { ConnectComponent } from './connect/connect.component';
 import { HomeComponent } from './home/home.component';
 import { GameComponent } from './game/game.component';
+
+const routes: Routes = [
+	{path: 'home', component: HomeComponent},
+	{path: 'game', component: GameComponent},
+	{path: '', redirectTo: 'home', pathMatch: 'full'},
+	{path: '**', redirectTo: 'home', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +23,8 @@ import { GameComponent } from './game/game.component';
     GameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})
   ],
   providers: [],
   bootstrap: [AppComponent]
